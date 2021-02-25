18792cb291a23764ec10ee3ba7a6e3cb6ea58027
alizare1 <ali.zare2000123@gmail.com>
2021-02-25 13:11:51 +0330

    Add project template

---
+++ src/test/java/com/marshmellow/CourseSelectionSystemTest.java
@@ -0,0 +1,12 @@
+package com.marshmellow;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class CourseSelectionSystemTest {
+
+    @Test
+    public void tempTest() {
+        assertEquals(2, 2);
+    }
+}
