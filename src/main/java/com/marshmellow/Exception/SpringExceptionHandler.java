a77b053bafd65977148404e1ddcfbfa7c41bf862
alizare1 <ali.zare2000123@gmail.com>
2021-04-18 19:15:28 +0430

    add exception handler for spring rest

---
+++ src/main/java/com/marshmellow/Exception/SpringExceptionHandler.java
@@ -0,0 +1,31 @@
+package com.marshmellow.Exception;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class SpringExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler({OfferingNotFound.class, StudentNotFound.class})
+    public ResponseEntity<Map<String, String>> handleNotFoundException(Exception ex, WebRequest request) {
+        Map<String, String> response = new HashMap<>();
+        response.put("error", ex.getMessage());
+        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler({Exception.class})
+    public ResponseEntity<Map<String, String>> handleExceptions(Exception ex, WebRequest request) {
+        Map<String, String> response = new HashMap<>();
+        response.put("error", ex.getMessage());
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+}
