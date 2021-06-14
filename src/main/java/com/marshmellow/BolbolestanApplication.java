9ac2bf5a37c9105ef48f695bef0ae1200e538193
Ali Zare <ali.zare2000123@gmail.com>
2021-06-14 13:06:31 +0000

    add cd

--- src/main/java/com/marshmellow/BolbolestanApplication.java
+++ src/main/java/com/marshmellow/BolbolestanApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BolbolestanApplication {
 
 	public static void main(String[] args) {
+		System.setProperty("server.port", "8080");
 		SpringApplication.run(BolbolestanApplication.class, args);
 	}
 
